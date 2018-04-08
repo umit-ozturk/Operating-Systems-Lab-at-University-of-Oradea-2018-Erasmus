33b1ffe9bacf4f0bfbe9c3c19d989759a45519f8
Ümit <umit.ozturk.9716@gmail.com>
2018-04-08 21:52:44 +0300

    Created Cliend and Server Socket

--- client_tcp.c
+++ client_tcp.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h> 		/* for close() */
 
 
 int main(){
@@ -13,26 +14,27 @@ int main(){
 	network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	// Configration Address for The Socket 
-	struct sockaddr_in server_addr;
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(9002);
-	server_addr.sin_addr.s_addr = INADDR_ANY;
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9002);
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	// Connection
-	int con_status = connect(network_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
+	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	printf("%d\n", connection_status);
 
-	if (con_status == -1){
+	if (connection_status == -1){
 		printf("Something went wrong \n\n");
 	}
 
 
 	// receive data from the server
-	char server_resp[256];
-	recv(network_socket, &server_resp, sizeof(server_resp), 0);
+	char server_response[256];
+	recv(network_socket, &server_response, sizeof(server_response), 0);
 
-	printf("Receive from server --> %s\n", server_resp);
-
+	printf("Receive from server --> %s\n", server_response);
 
 	close(network_socket);
+	
 	return 0;
 }
--- server_tcp.c
+++ server_tcp.c
@@ -4,19 +4,35 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h> 		/* for close() */
 
 int main(){
-	char server_messg[256] = "You have received the server";
+	char server_message[256] = "You have received the server";
 
 	// Create Server Socket
 	int server_socket;
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	// Define The Server Address
-	struct sockaddr_in server_addr;
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(9002);
-	server_addr.sin_addr.s_addr = INADDR_ANY;
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9002);
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// Bind The Server Socket to Our IP and Port
+	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+	listen(server_socket, 5);
+
+	int client_socket;
+	client_socket = accept(client_socket, NULL, NULL);
+
+	// Send Message
+	send(client_socket, server_message, sizeof(server_message), 0);
+
+	// Close Socket
+	
+	close(server_socket);
 
 	return 0;
 }
