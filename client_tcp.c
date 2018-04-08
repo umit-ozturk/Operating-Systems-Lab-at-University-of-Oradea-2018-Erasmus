9b9bbb26ce9d776d1e6b82b1bf5562d44941cc1e
Ümit <umit.ozturk.9716@gmail.com>
2018-04-08 19:24:11 +0300

    Create a Socket and Congration Address for the Socket and Try Connect

--- client_tcp.c
+++ client_tcp.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 
 int main(){
+
+	// Create Network Socket
+	int network_socket;
+	network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// Configration Address for The Socket 
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(9002);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	// Connection
+	int con_status = connect(network_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
+
+	if (con_status == -1){
+		printf("Something went wrong");
+	}
 
 	return 0;
 }
